da6db87442ba1c4db7f1e3e5eb7a34236635ff3d
matthewrostkowski <69280162+matthewrostkowski@users.noreply.github.com>
2022-11-01 04:04:52 -0400

    Update list.js

--- list.js
+++ list.js
@@ -1,1 +1,28 @@
 
+const itemList = [{name: "Firework", email: "Katyperry@columbia.edu"}, 
+                    {name: "Hamilton", email: "hamilton@columbia.edu"}, 
+                    {name: "Kyle", email: "kyle@columbia.edu"}]
+
+function List(){
+    return(
+        <div class="List">
+            {itemList.map((info, index) => (
+            <ListItem index={index+1} name={info.name} email={info.email} />
+            ))}
+            
+        </div>
+        
+    );
+}
+export default List;
+function ListItem({index, name, email}){
+    return(
+        <div class="list-item">
+          <div class="item item-number">{index}</div>
+          <div class="item">{name}</div>
+          <div class="item-email">{email}</div>
+          <button class="delete-button">DELETE</button>
+        </div>            
+    )
+
+}
