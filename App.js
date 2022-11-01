bac892e1327f8b2b8290368b0685f6453600b5d7
matthewrostkowski <69280162+matthewrostkowski@users.noreply.github.com>
2022-11-01 04:05:36 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+ import './App.css';
 import Form from './Components/Form';
 import List from './Components/List';
-function App() {
-  return (
-    <div class="App">
-      <div class="logo">
-        <img src="https://clubs-cu.s3.amazonaws.com/Spectator+Publishing+Logo.png" alt="spectator publishing company logo" width="263" height="55"></img>
-      </div>  
-      <div class="Header">Spectator's Sources</div>
+ function App() {
+   return (
 
+     <div class="App">
+
+       <div class="Header">Spectator's Sources</div>
       <Form/>
       <List/>
+       
 
+     </div>
 
+   );
+ }
 
-    </div>
-  
-  );
-}
-
-export default App;
+ export default App;
