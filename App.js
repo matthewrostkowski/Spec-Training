3eaa51678b385a931a21137dfb92255b5ffa3a08
matthewrostkowski <69280162+matthewrostkowski@users.noreply.github.com>
2022-11-01 02:30:57 -0400

    Update App.js
    up to vid 3

--- App.js
+++ App.js
@@ -1,51 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Form from './Components/Form';
+import List from './Components/List';
 function App() {
   return (
-      
     <div class="App">
       <div class="logo">
         <img src="https://clubs-cu.s3.amazonaws.com/Spectator+Publishing+Logo.png" alt="spectator publishing company logo" width="263" height="55"></img>
       </div>  
       <div class="Header">Spectator's Sources</div>
 
-      <form class="Form">
-        <span>
-          <span>Source Name:</span>
-          <input class="form-input" type="text" id="name" placeholder=" "></input>
-        </span>
-        <span>
-          <span>Source Email:</span>
-          <input class="form-input" type="text" id="name" placeholder=" "></input>
-        </span>
-        <button class="form-button">ADD</button>
-      </form>
+      <Form/>
+      <List/>
 
-      <div class="list">
 
-        <div class="list-item">
-          <div class="item item-number">1</div>
-          <div class="item">Presbo</div>
-          <div class="item-email">presbo@columbia.edu</div>
-          <button class="delete-button">DELETE</button>
-        </div>
-
-        <div class="list-item">
-          <div class="item item-number">2</div>
-          <div class="item">John Jay Mouse</div>
-          <div class="item-email">mouse@columbia.edu</div>
-          <button class="delete-button">DELETE</button>
-        </div>
-
-        <div class="list-item">
-          <div class="item item-number">1</div>
-          <div class="item">Water Bottle Man</div>
-          <div class="item-email">flipper@columbia.edu</div>
-          <button class="delete-button">DELETE</button>
-        </div>
-        
-      </div>
 
     </div>
   
